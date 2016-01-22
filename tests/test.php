893da25e9c481eb983876c2c86c811166312163e
Michael Steele <michael@steeleimage.com>
2016-01-22 10:05:17 -0800

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 use Analytics\Ga;
 
 echo Ga::world();
-//Go to the terminal (or create a PHP web server inside "tests" dir) and type:
+//Go to the terminal (or create a PHP web server inside "tests" dir) and type: 
 //php tests/test.php
 
 ?>
